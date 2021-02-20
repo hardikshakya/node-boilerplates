9b3e27565c6cb76fa6f320c328ce2406a11a9d82
hardikshakya <hardikshakya1997@gmail.com>
2021-02-20 12:06:57 +0530

    T2: Setup Eslint (airbnb), Prettier and Husky

--- node-ts/src/common/constants/http-status.ts
+++ node-ts/src/common/constants/http-status.ts
@@ -1,3 +1,7 @@
+/* eslint-disable lines-between-class-members */
+// eslint-disable-next-line import/prefer-default-export
 export class HttpStatus {
   public static OK = 200;
+
+  public static BAD_REQUEST = 400;
 }
--- node-ts/src/index.ts
+++ node-ts/src/index.ts
@@ -1,9 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 // Load HTTP status
 import { HttpStatus } from './common/constants';
 // Load env vars
 import loadEnv from './common/config/env';
+
 loadEnv();
 
 // Boot express
@@ -11,9 +12,10 @@ const app: Application = express();
 const port = process.env.PORT;
 
 // Application routing
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
+app.use('/', (req: Request, res: Response) => {
   res.status(HttpStatus.OK).send({ data: 'Hello, Hardik Here :)' });
 });
 
 // Start server
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
